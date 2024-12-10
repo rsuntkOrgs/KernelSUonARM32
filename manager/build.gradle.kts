55d49bb61a82fba3e612d62966f10f3a34d2cdf6
rsuntk <rissu.ntk@gmail.com>
2024-12-10 16:17:44 +0700

    manager: force armv7
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -80,7 +80,7 @@ subprojects {
                     versionName = managerVersionName
                 }
                 ndk {
-                    abiFilters += listOf("arm64-v8a", "x86_64", "riscv64")
+                    abiFilters += listOf("armeabi-v7a")
                 }
             }
 
